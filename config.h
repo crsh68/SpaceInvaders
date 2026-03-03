a1813ce04aa2beaaad9355cdc5868dc4b9c16043
crsh68 <gm.crsh@gmail.com>
2026-03-03 12:14:38 +0100

    Add files via upload

--- config.h
+++ config.h
@@ -17,27 +17,27 @@
 //   GPIO21 = DIN
 //
 // KONTROLE:
-//   GPIO0  = START (BOOT tipka)
+//   GPIO0  = START 
 //   GPIO2  = LEFT
 //   GPIO3  = RIGHT
 //   GPIO10 = COIN
-//   GPIO9  = FIRE
+//   GPIO9  = FIRE (BOOT tipka)
 // =========================================
 
 #define PIN_LEFT     2
 #define PIN_RIGHT    3
 #define PIN_FIRE     9
 #define PIN_COIN     10
-#define PIN_START_P1 0
+#define PIN_START_P1 8
 
 #define P1_LEFT   2
 #define P1_RIGHT  3
 #define P1_SHOOT  9
 #define COIN      10
-#define P1_START  0
+#define P1_START  8
 #define P2_LEFT   5
 #define P2_RIGHT  6
 #define P2_SHOOT  7
-#define P2_START  8
+#define P2_START  0
 
 #endif
